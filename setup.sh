f34ea8f37a6138766d129d00c3599f1601b7b16e
shanecuster <83169616+shanecuster@users.noreply.github.com>
2024-01-04 14:40:20 -0500

    Update setup.sh
    I updated the documentation a bit more on this file with comments. I ran this on a virtual machine and the output was all put together so I added some blank lines to clean up the output on the screen.

--- setup.sh
+++ setup.sh
@@ -4,18 +4,48 @@
 #I use this with Fedora but it can be altered for other distributions.
 #Rough draft of this. I will work on it more as I go.
 
-
 sudo hostnamectl set-hostname "Fedora"
 
 #Editing the DNF configuration file.
-echo "max_parallel_downloads=10" >> /etc/dnf/dnf.conf
-echo "fastestmirror=true" >> /etc/dnf/dnf.conf
-echo "defaultyes=true" >> /etc/dnf/dnf.conf
+sudo echo "max_parallel_downloads=10" >> /etc/dnf/dnf.conf
+sudo echo "fastestmirror=true" >> /etc/dnf/dnf.conf
+sudo echo "defaultyes=true" >> /etc/dnf/dnf.conf
+
+#Adding a blank line in text for cleaner output.
+echo -e "\n"
+
+#Installing DNF5
+sudo dnf install dnf5
+
+#Creating Aliases for DNF5
+sudo echo "alias dnf="dnf5"" >> /home/.bashrc
+sudo echo "alias sudo="sudo"" >> /home/.bashrc
+
+#Notification of DNF5 Install and Aliases created.
+echo "DNF5 has been installed and your aliases have been created."
+
+#Adding a blank line in text for cleaner output.
+echo -e "\n"
+
+#Alerting the user that the upgrade will start.
+echo "System upgrade will now begin."
 
 #Updating the System.
 sudo dnf upgrade
 
+#Adding a blank line in text for cleaner output.
+echo -e "\n"
+
+#Alerting the user the system has been upgraded.
+echo "Your system has been updated."
+
+#Adding a blank line in text for cleaner output.
+echo -e "\n"
+
 #Enabling RPM Fusion repository.
+
+#Alerting User that RPM Fusion will be enabled.
+echo "Enabling RPM Fusion Repositories."
 
 #Free
 sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
@@ -23,8 +53,27 @@ sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-rele
 #Non-Free
 sudo dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
+#Alerting user that RPM Fusion has been added.
+echo "RPM Fusion Repositories have been added."
+
+#Adding a blank like in text for cleaner output.
+echo -e "\n"
+
 #Enabling the Flatpak Repository.
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+
+#Adding a blank line in text for cleaner output.
+echo -e "\n"
+
+#Alerting user that Flatpak Repository has been added.
+echo "Flatpak Repository has been added."
+
+#Adding a blank line in text for cleaner output.
+echo -e "\n"
+
+#I use Brave Browser and Bitwarden on every machine to these will always be installed.
+#Alerting the user Brave Browser will be installed.
+echo "Now installing Brave Browser."
 
 #Installing Brave Browser.
 sudo dnf install dnf-plugins-core
@@ -35,15 +84,49 @@ sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
 
 sudo dnf install brave-browser
 
-#Installing Bitwarden password manager.
+#Adding a blank line in text for cleaner output.
+echo -e "\n"
+
+#Alerting the user that Brave Browser has been installed.
+echo "Brave Browser has been installed."
+
+#Adding a blank line in text for cleaner output.
+echo -e "\n"
+
+#Alerting the user that Bitwarden is being installed.
+echo "Now installing Bitwarden Password Manager."
+
+#Installing Bitwarden Password manager.
 sudo dnf install bitwarden -y
+
+#Adding a blank line in text for cleaner output.
+echo -e "\n"
+
+#Alerting the user Bitwarden has been installed.
+echo "Bitwarden Password Manager has been installed."
+
+#Adding a blank line in text for cleaner output.
+echo -e "\n"
+
+#Alering the user some other utilities and Applications will be installed.
+#This will be updated as I go on. This section is for applications I use regularly.
+echo "Now installing other Applications and Utilities."
 
 #Installing Neofetch and setting it to run at terminal startup.
 sudo dnf install neofetch -y
 
-echo "neofetch" >> /home/.bashrc
+sudo echo "neofetch" >> /home/.bashrc
 
 #Installing Htop and Stacer
 sudo dnf install htop stacer -y
+
+#Installing Discord
+sudo dnf install discord -y
+
+#Clearing the screen after install.
+clear
+
+#Setting Neofetch to run after the screen is cleared to appear as a brand new terminal prompt.
+neofetch
 
 exit
