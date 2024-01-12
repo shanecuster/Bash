92452b78b2d1ca48b9806e1b06392d65034f15c5
shanecuster <83169616+shanecuster@users.noreply.github.com>
2024-01-12 07:52:50 -0500

    Update setup.sh
    I changed All of the software packages that I could to be installed via Flathub. 
    
    Added some Comments for output after each software package was installed.

--- setup.sh
+++ setup.sh
@@ -22,13 +22,13 @@ sudo echo "alias dnf="dnf5"" >> /home/.bashrc
 sudo echo "alias sudo="sudo"" >> /home/.bashrc
 
 #Notification of DNF5 Install and Aliases created.
-echo "DNF5 has been installed and your aliases have been created."
+echo "DNF5 has been installed and your aliases have been created." && \
 
 #Adding a blank line in text for cleaner output.
 echo -e "\n"
 
 #Alerting the user that the upgrade will start.
-echo "System upgrade will now begin."
+echo "System upgrade will now begin." && \
 
 #Updating the System.
 sudo dnf upgrade -y
@@ -37,7 +37,7 @@ sudo dnf upgrade -y
 echo -e "\n"
 
 #Alerting the user the system has been upgraded.
-echo "Your system has been updated."
+echo "Your system has been updated." && \
 
 #Adding a blank line in text for cleaner output.
 echo -e "\n"
@@ -45,7 +45,7 @@ echo -e "\n"
 #Enabling RPM Fusion repository.
 
 #Alerting User that RPM Fusion will be enabled.
-echo "Enabling RPM Fusion Repositories."
+echo "Enabling RPM Fusion Repositories." && \
 
 #Free
 sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
@@ -54,7 +54,7 @@ sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-rele
 sudo dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
 #Alerting user that RPM Fusion has been added.
-echo "RPM Fusion Repositories have been added."
+echo "RPM Fusion Repositories have been added." && \
 
 #Adding a blank like in text for cleaner output.
 echo -e "\n"
@@ -66,225 +66,351 @@ flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.f
 echo -e "\n"
 
 #Alerting user that Flatpak Repository has been added.
-echo "Flatpak Repository has been added."
+echo "Flatpak Repository has been added." && \
 
 #Adding a blank line in text for cleaner output.
 echo -e "\n"
 
-#I use Brave Browser and Bitwarden on every machine to these will always be installed.
-#Alerting the user Brave Browser will be installed.
-echo "Now installing Brave Browser."
+#Alerting the user that Software will be now be installed.
+echo "Now installing Software Packages." && \
 
-#Installing Brave Browser.
-sudo dnf install dnf-plugins-core
+#Web Browsers
+#Firefox is installed by default.
 
-sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
-
-sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
-
-sudo dnf install brave-browser
+#Installing Brave Browser via Flathub.
+flatpak install flathub com.brave.Browser -y
+echo "Brave Browser has been installed." && \
 
 #Adding a blank line in text for cleaner output.
 echo -e "\n"
 
-#Alerting the user that Brave Browser has been installed.
-echo "Brave Browser has been installed."
+#Installing Librewolf via Flathub
+flatpak install flathub io.gitlab.librewolf-community -y
+echo "Librewolf has been installed." && \
 
 #Adding a blank line in text for cleaner output.
 echo -e "\n"
 
-#Alerting the user that Bitwarden is being installed.
-echo "Now installing Bitwarden Password Manager."
+#Installing Vivaldi via Flathub
+flatpak install flathub com.vivaldi.Vivaldi -y
+echo "Vivaldi has been installed." && \
+
+#Adding a blank line in text for cleaner output.
+echo -e "\n"
 
 #Installing Bitwarden Password manager.
 flatpak install flathub com.bitwarden.desktop -y
+echo "Bitwarden Password Manager has been installed." && \
 
 #Adding a blank line in text for cleaner output.
 echo -e "\n"
-
-#Alerting the user Bitwarden has been installed.
-echo "Bitwarden Password Manager has been installed."
-
-#Adding a blank line in text for cleaner output.
-echo -e "\n"
-
-#Alering the user some other utilities and Applications will be installed.
-#This will be updated as I go on. This section is for applications I use regularly.
-echo "Now installing other Applications and Utilities."
 
 #Installing Neofetch and setting it to run at terminal startup.
 sudo dnf install neofetch -y
+echo "Neofetch has been installed." && \
+
+#Adding a blank line in text for cleaner output.
+echo -e "\n"
 
 sudo echo "neofetch" >> /home/.bashrc
 
-#Installing Some Frequently Used Utilities.
-
 #Installing Htop
 sudo dnf install htop -y
+echo "HTOP has been installed." && \
+
+#Adding a blank line in text for cleaner output.
+echo -e "\n"
 
 #Installing Stacer
 sudo dnf install stacer -y
+echo "Stacer has been installed." && \
 
-#Installing Some Flatpak's via Flathub.
+#Adding a blank line in text for cleaner output.
+echo -e "\n"
 
 #PrusaSlicer for 3D Printer.
 
 #Installing PrusaSlicer via Flathub
 flatpak install flathub com.prusa3d.PrusaSlicer -y
+echo "PrusaSlicer has been installed." && \
+echo "Have fun 3D Printing!" && \
+
+#Adding a blank line in text for cleaner output.
+echo -e "\n"
 
 #Other Applications for 3D Printing.
 
 #Installing FreeCAD via Flathub
 flatpak install flathub org.freecadweb.FreeCAD -y
+echo "FreeCAD has been installed." && \
+
+#Adding a blank line in text for cleaner output.
+echo -e "\n"
 
 #Installing Blender via Flathub
 flatpak install flathub org.blender.Blender -y
+echo "Blender has been installed." && \
+
+#Adding a blank line in text for cleaner output.
+echo -e "\n"
 
 #Chat Applications
 
 #Installing Discord via Flathub
 flatpak install flathub com.discordapp.Discord -y
+echo "Discord has been installed." && \
+
+#Adding a blank line in text for cleaner output.
+echo -e "\n"
 
 #Installing Telegram via Flathub
 flatpak install flathub org.telegram.desktop -y
+echo "Telegram has been installed." && \
+
+#Adding a blank line in text for cleaner output.
+echo -e "\n"
 
 #Installing Whalebird via Flathub
 flatpak install flathub social.whalebird.WhalebirdDesktop -y
+echo "Whalebird has been installed." && \
+
+#Adding a blank line in text for cleaner output.
+echo -e "\n"
 
 #Installing HexChat via Flathub
 flatpak install flathub io.github.Hexchat -y
+echo "HexChat has been installed." && \
+
+#Adding a blank line in text for cleaner output.
+echo -e "\n"
 
 #Music Players
 
 #Installing Cider via Flathub
 flatpak install flathub sh.cider.Cider -y
+echo "Cider has been installed." && \
+
+#Adding a blank line in text for cleaner output.
+echo -e "\n"
 
 #Installing Blanket via Flathub
 flatpak install flathub com.rafaelmardojai.Blanket -y
+echo "Blanket has been installed." && \
+
+#Adding a blank line in text for cleaner output.
+echo -e "\n"
 
 #Installing Podcasts via Flathub
 flatpak install flathub org.gnome.Podcasts -y
+echo "Podcasts has been installed." && \
+
+#Adding a blank line in text for cleaner output.
+echo -e "\n"
 
 #Installing Shortwave via Flathub
 flatpak install flathub de.haeckerfelix.Shortwave -y
+echo "Shortwave has been installed." && \
+
+#Adding a blank line in text for cleaner output.
+echo -e "\n"
 
 #Sublime for Text editing and Git.
 
 #Installing Sublime Text via Flathub
 flatpak install flathub com.sublimetext.three -y
+echo "Sublime Text has been installed." && \
+
+#Adding a blank line in text for cleaner output.
+echo -e "\n"
 
 #Installing Sublime Merge via Flathub
 flatpak install flathub com.sublimemerge.App -y
+echo "Sublime Merge has been installed." && \
+
+#Adding a blank line in text for cleaner output.
+echo -e "\n"
 
 #IDE's for Code editing
 
 #Installing Visual Studio Code via Flathub
 flatpak install flathub com.visualstudio.code -y
+echo "Visual Studio Code has been installed." && \
+
+#Adding a blank line in text for cleaner output.
+echo -e "\n"
 
 #Installing Pycharm Community via Flathub
 flatpak install flathub com.jetbrains.PyCharm-Community -y
+echo "Pycharm has been installed." && \
+
+#Adding a blank line in text for cleaner output.
+echo -e "\n"
 
 #Deja Dup for Backups
 
 #Installing Deja Dup via Flathub
 flatpak install flathub org.gnome.DejaDup -y
+echo "Deja Dup has been installed." && \
+
+#Adding a blank line in text for cleaner output.
+echo -e "\n"
 
 #Torrents
 
 #Installing Fragments via Flathub
 flatpak install flathub de.haeckerfelix.Fragments -y
+echo "Fragments has been installed." && \
+
+#Adding a blank line in text for cleaner output.
+echo -e "\n"
 
 #DVD/Blu-Ray Conversion
 
 #Installing MakeMKV via Flathub
 flatpak install flathub com.makemkv.MakeMKV -y
+echo "MakeMKV has been installed." && \
 
-#Installing Handbrake
-sudo dnf install handbrake -y
+#Adding a blank line in text for cleaner output.
+echo -e "\n"
+
+#Installing Handbrake via Flathub.
+flatpak install flathub fr.handbrake.ghb -y
+echo "Handbrake has been installed." && \
+
+#Adding a blank line in text for cleaner output.
+echo -e "\n"
 
 #Digital Art
 
 #Installing Krita via Flathub
 flatpak install flathub org.kde.krita -y
+echo "Krita has been installed." && \
+
+#Adding a blank line in text for cleaner output.
+echo -e "\n"
 
 #Installing Gimp via Flathub
 flatpak install flathub org.gimp.GIMP -y
+echo "GIMP has been installed." && \
+
+#Adding a blank line in text for cleaner output.
+echo -e "\n"
 
 #Installing Inkscape via Flathub
 flatpak install flathub org.inkscape.Inkscape -y
+echo "Inkscape has been installed." && \
+
+#Adding a blank line in text for cleaner output.
+echo -e "\n"
 
 #Virtual Machine Software
 
 #Installing Virtual Machine Manager
 sudo dnf install virt-manager -y
+echo "Virtual Machine Manager has been installed." && \
 
-#Installing Boxes via Flathub
-flatpak install flathub org.gnome.Boxes -y
+#Adding a blank line in text for cleaner output.
+echo -e "\n"
 
 #Email Clients
 
 #Installing Geary via Flathub
 flatpak install flathub org.gnome.Geary -y
+echo "Geary has been installed." && \
+
+#Adding a blank line in text for cleaner output.
+echo -e "\n"
 
 #Installing Evolution via Flathub
 flatpak install flathub org.gnome.Evolution -y
+echo "Evolution has been installed." && \
+
+#Adding a blank line in text for cleaner output.
+echo -e "\n"
 
 #Installing Thunderbird via Flathub
 flatpak install flathub org.mozilla.Thunderbird -y
+echo "Thunderbird has been installed." && \
 
-#Web Browsers
-#Firefox is installed by default.
-#Brave should already be installed at this point.
-
-#Installing Librewolf via Flathub
-flatpak install flathub io.gitlab.librewolf-community -y
-
-#Installing Vivaldi via Flathub
-flatpak install flathub com.vivaldi.Vivaldi -y
+#Adding a blank line in text for cleaner output.
+echo -e "\n"
 
 #Game Stuff
 
 #Installing Steam via Flathub
 flatpak install flathub com.valvesoftware.Steam -y
+echo "Steam has been installed." && \
+
+#Adding a blank line in text for cleaner output.
+echo -e "\n"
 
 #Installing Bottles via Flathub
 flatpak install flathub com.usebottles.bottles -y
+echo "Bottles has been installed." && \
+
+#Adding a blank line in text for cleaner output.
+echo -e "\n"
 
 #Installing MiniGalaxy via Flathub
 flatpak install flathub io.github.sharkwouter.Minigalaxy -y
+echo "MiniGalaxy has been installed." && \
+
+#Adding a blank line in text for cleaner output.
+echo -e "\n"
 
 #Installing Heroic Games Launcher via Flathub
 flatpak install flathub com.heroicgameslauncher.hgl -y
+echo "Heroic Games Launcher has been installed." && \
+
+#Adding a blank line in text for cleaner output.
+echo -e "\n"
 
 #Installing Lutris via Flathub
 flatpak install flathub net.lutris.Lutris -y
+echo "Lutris has been installed." && \
+
+#Adding a blank line in text for cleaner output.
+echo -e "\n"
 
 #Books
 
 #Installing Calibre via Flathub
 flatpak install flathub com.calibre_ebook.calibre -y
+echo "Calibre has been installed." && \
+
+#Adding a blank line in text for cleaner output.
+echo -e "\n"
 
 #Photos
 
 #Installing XnConvert via Flathub
 #This is what you use for converting WebP Images for Wallpapers.
 flatpak install flathub com.xnview.XnConvert -y
+echo "XnConvert has been installed." && \
+
+#Adding a blank line in text for cleaner output.
+echo -e "\n"
 
 #Remote Desktop
 
 #Installing Remmina via Flathub
 flatpak install flathub org.remmina.Remmina -y
+echo "Remmina has been installed." && \
+
+#Adding a blank line in text for cleaner output.
+echo -e "\n"
 
 #Other Stuff that needs to be installed.
 
 #Installing Authenticator via Flathub
 flatpak install flathub com.belmoussaoui.Authenticator -y
+echo "Authenticator has been installed." && \
 
 #Adding a blank line in text for cleaner output.
 echo -e "\n"
 
 #Alerting the user that their software has been installed.
-echo "Software has been installed."
+echo "Software has been installed." && \
 
 #Adding a blank line in text for cleaner output.
 echo -e "\n"
