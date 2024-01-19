c2c1bb5e53f34d9fc44e3933ed8acd73e0725d72
shanecuster <83169616+shanecuster@users.noreply.github.com>
2024-01-18 20:53:19 -0500

    Create apt_upgrade.sh
    This is a brand new script that I will use to update a Debian machine. This will not be run very often but I am putting it together for testing/learning.

---
+++ apt_upgrade.sh
@@ -0,0 +1,17 @@
+#I have setup my laptop with Debian to try something new.
+#I will make a setup script for that distriibution as well.
+#This is my learning process for the apt package manager.
+#I am far more familiar with Fedora than Debian so this is necessary.
+
+#Welcome Message
+echo "Hello Shane, Lets get your system updated for you."
+
+#Update Command
+sudo apt update && sudo apt upgrade -y
+
+#Update successful message
+echo "Debian has been updated."
+
+clear
+
+exit
