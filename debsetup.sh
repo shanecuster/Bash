1adbdf45ab85ca884e50222b344638c0a348b370
shanecuster <83169616+shanecuster@users.noreply.github.com>
2024-01-28 13:13:55 -0500

    Update and rename setup_debian.sh to debsetup.sh
    Beginning to edit the Debian Setup file. Here I am installing Nala.

---
+++ debsetup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#This is my Debian setup script.
+#I will edit this as I go.
+
+
+#Installing Nala
+echo "deb [arch=amd64,arm64,armhf] http://deb.volian.org/volian/ scar main" | sudo tee /etc/apt/sources.list.d/volian-archive-scar-unstable.list
+wget -qO - https://deb.volian.org/volian/scar.key | sudo tee /etc/apt/trusted.gpg.d/volian-archive-scar-unstable.gpg > /dev/null
+sudo apt update && sudo apt install nala -y
--- setup_debian.sh
+++
@@ -1,3 +0,0 @@
-#This is my Debian setup file.
-#I am working through the steps right now that I will add to this.
-#Working this one out as I go.
