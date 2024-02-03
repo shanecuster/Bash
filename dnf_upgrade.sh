976180788639bfe234376707918c61ffa6c6033e
shanecuster <83169616+shanecuster@users.noreply.github.com>
2024-02-03 15:46:39 -0500

    Update dnf_upgrade.sh
    If seconds are greater than 60. I have it convert to minutes.

--- dnf_upgrade.sh
+++ dnf_upgrade.sh
@@ -55,8 +55,14 @@ end_time=$(date +%s)
 #Calculating the time that elapsed.
 elapsed_time=$((end_time - start_time))
 
-#Displaying the elapsed time.
-echo "Your upgrade took $elapsed_time seconds."
+# Convert to minutes and seconds if elapsed time is greater than 60 seconds
+if ((elapsed_time >= 60)); then
+    minutes=$((elapsed_time / 60))
+    seconds=$((elapsed_time % 60))
+    echo "Script completed in $minutes minutes and $seconds seconds."
+else
+    echo "Script completed in $elapsed_time seconds."
+fi
 
 #Adding a blank line in text for cleaner output.
 echo -e "\n"
